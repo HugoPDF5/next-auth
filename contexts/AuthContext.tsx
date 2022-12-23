dbfc995a0c7ca5790f79e65cc1a54983c18c09c1
Hugo <hugopatricio51@gmail.com>
2022-12-23 17:27:12 -0300

    feat: adding cookie feature

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+import { setCookie } from 'nookies'
+import  Router  from "next/router";
+
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[]
+}
 
 type SignInCredentials = {
     email: string;
@@ -7,6 +16,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
     signIn(credentials: SignInCredentials): Promise<void>,
+    user: User,
     isAuthenticated: boolean
 }
 
@@ -17,15 +27,45 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<User>()
+    const isAuthenticated = !!user
 
-    const isAuthenticated = false
+    useEffect(() => {
+        
+    }, [])
 
     async function signIn({ email, password }: SignInCredentials) {
+        try {
+            const response = await api.post('sessions', {
+                email,
+                password
+            })
 
+            const { token, refreshToken, permissions, roles } = response.data
+
+            setCookie(undefined, 'next-auth.token', token, {
+                maxAge: 60 * 60,
+                path: '/'
+            })
+            setCookie(undefined, 'next-auth.refreshToken', refreshToken, {
+                maxAge: 60 * 60,
+                path: '/'
+            })
+
+            setUser({
+                email,
+                permissions,
+                roles
+            })
+
+            Router.push('/dashboard')
+        } catch (err) {
+            console.error(err)
+        }
     }
 
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
     )
